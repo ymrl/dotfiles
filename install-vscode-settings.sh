9fbc4060fb705c340b2b5b408f731c73ab1ffb2b
ymrl <ymrl@ymrl.net>
2018-01-08 21:16:41 +0900

    add VSCode Extensions

--- install-vscode-settings.sh
+++ install-vscode-settings.sh
@@ -2,3 +2,8 @@
 
 ln -s "`pwd`/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 ln -s "`pwd`/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+cat "`pwd`/vscode/extensions" | while read line
+do
+  code --install-extension $line
+done
