7d20b461d53aef756b9dfaede24d7ce7ffb0ac37
ymrl <ymrl@ymrl.net>
2018-01-08 21:11:16 +0900

    add VSCode Settings

---
+++ install-vscode-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s "`pwd`/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+ln -s "`pwd`/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
